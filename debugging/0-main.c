f4865de2de03303573ae04562454114a462d963f
nicolas1897 <5217@holbertonschool.com>
2022-06-17 11:15:20 -0700

    updates

--- debugging/0-main.c
+++ debugging/0-main.c
@@ -1,27 +1,14 @@
-#include <stdlib.h>
-  #include <main.h>
-#include <stdio.h>
+#include "main.h"
 
-
-
-/**                                                              *main - Entry point                                              *Return: Always 0 (Success)                                      */
+/**
+* main - tests function prints if integer positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%i is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-printf("%i is negative\n", n);
-}
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
