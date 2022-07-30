d44e84d97ee93cf35fcf9839afd1225efc49cc28
nicolas1897 <5217@holbertonschool.com>
2022-07-30 14:05:25 -0700

    updates

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,62 +1,41 @@
 #include "lists.h"
 
-listint_t *create_new_node(int n);
-
 /**
-* insert_nodeint_at_index - Inserts a node at a given index
-* @head: Pointer to the first element of the list
-* @idx: Index to insert a node at
-* @n: number to be inserted into the node
-* Return: The address of the newly created node
+* insert_nodeint_at_index - insert a new node at a given position
+* @head: pointer to head pointer of linked list
+* @idx: index to insert new node
+* @n: new node's data
+* Return: address of new node, or NULL if failed
 */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i;
-listint_t *tmp;
-listint_t *tmp_old;
-listint_t *new_node;
-tmp = *head;
+unsigned int i = 0;
+listint_t *new_node, *tmp;
 if (head == NULL)
 return (NULL);
-new_node = create_new_node(n);
-if (new_node == NULL)
-return (NULL);
-if (*head == NULL)
-{
-*head = new_node;
-return (new_node);
-}
-if (idx == 0)
-*head = new_node;
-for (i = 0; i < idx - 1 && tmp != NULL && idx != 0; i++)
-tmp = tmp->next;
-if (tmp == NULL)
-return (NULL);
-if (idx == 0)
-new_node->next = tmp;
-else
-{
-tmp_old = tmp->next;
-tmp->next = new_node;
-new_node->next = tmp_old;
-}
-return (new_node);
-}
-
-/**
-* create_new_node - Creates a new node
-* @n: Value to add to new node
-* Return: A pointer to a node
-*/
-
-listint_t *create_new_node(int n)
-{
-listint_t *new_node;
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
-return (NULL)
+return (NULL);
 new_node->n = n;
-new_node->next = NULL;
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
+tmp = *head;
+while (i < (idx - 1))
+{
+tmp = tmp->next;
+i++;
+if (tmp == NULL)
+{
+free(new_node);
+return (NULL);
+}
+}
+new_node->next = tmp->next;
+tmp->next = new_node;
 return (new_node);
 }
